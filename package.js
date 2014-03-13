1de1ca88494573392be783cd34a6ed97d9ab6f9c
voodoohop <t.haferlach@gmail.com>
2014-03-13 19:38:04 -0300

    forgot imagesloaded

--- package.js
+++ package.js
@@ -12,7 +12,7 @@ Package.on_use(function (api) {
     'ui']
     , 'client');
 
-  api.add_files(['packery/packery.pkgd.min.js', 'packery/imagesloaded.js'], 'client');
+  api.add_files(['packery/packery.pkgd.min.js', 'imagesloaded/imagesloaded.pkgd.js'], 'client');
   api.add_files(['template.html'], 'client');
   api.add_files(['masonry_style.css'], 'client');
   api.add_files(['masonrify.coffee'], 'client');
